3bbff697c1ea4442342abd768835d24497e7ae4e
Jakob Pennington <jakobpennington@gmail.com>
2018-11-04 15:39:34 +1030

    build(environments.ts): Updated production API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    API_URL: 'https://ec2-13-210-142-46.ap-southeast-2.compute.amazonaws.com',
+    API_URL: 'https://ec2-13-210-118-174.ap-southeast-2.compute.amazonaws.com',
     production: true
 };
