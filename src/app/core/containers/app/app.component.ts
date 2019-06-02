da6453b3cb956b18df96d09303b6ced605f40aca
Jakob Pennington <jakob@pennington.io>
2019-06-02 16:30:00 +0930

    feat(flinders-preparation): added demo payloads, update reverse tcp demos

--- src/app/core/containers/app/app.component.ts
+++ src/app/core/containers/app/app.component.ts
@@ -12,19 +12,23 @@ export class AppComponent {
     SECRET_SESSION_USERNAME: string = 'USERNAME';
     SECRET_SESSION_PASSWORD: string = 'PASSWORD';
 
+    USERNAME: string = 'JohnDoe';
+    PASSWORD: string = 'DogsNameWifesBirthday';
+    TOKEN: string = '4237a03763b1467cb978a09e8b90dd8f';
+
     constructor(private readonly cookieService: CookieService, public electronService: ElectronService) {
         // set up secrets for demos
-        cookieService.set(this.SECRET_SESSION_TOKEN, 'WHATEVER');
-        cookieService.set(this.SECRET_SESSION_USERNAME, 'WHATEVER');
-        cookieService.set(this.SECRET_SESSION_PASSWORD, 'WHATEVER');
+        cookieService.set(this.SECRET_SESSION_TOKEN, this.TOKEN);
+        cookieService.set(this.SECRET_SESSION_USERNAME, this.USERNAME);
+        cookieService.set(this.SECRET_SESSION_PASSWORD, this.PASSWORD);
 
-        localStorage.setItem(this.SECRET_SESSION_TOKEN, 'WHATEVER');
-        localStorage.setItem(this.SECRET_SESSION_USERNAME, 'WHATEVER');
-        localStorage.setItem(this.SECRET_SESSION_PASSWORD, 'WHATEVER');
+        localStorage.setItem(this.SECRET_SESSION_TOKEN, this.TOKEN);
+        localStorage.setItem(this.SECRET_SESSION_USERNAME, this.USERNAME);
+        localStorage.setItem(this.SECRET_SESSION_PASSWORD, this.PASSWORD);
 
-        sessionStorage.setItem(this.SECRET_SESSION_TOKEN, 'WHATEVER');
-        sessionStorage.setItem(this.SECRET_SESSION_USERNAME, 'WHATEVER');
-        sessionStorage.setItem(this.SECRET_SESSION_PASSWORD, 'WHATEVER');
+        sessionStorage.setItem(this.SECRET_SESSION_TOKEN, this.TOKEN);
+        sessionStorage.setItem(this.SECRET_SESSION_USERNAME, this.USERNAME);
+        sessionStorage.setItem(this.SECRET_SESSION_PASSWORD, this.PASSWORD);
     }
 
     onClickMinimise(): void {
