9e8dbfd83a8d98cbec2fc63a14b70310afc426d6
Jakob Pennington <jakob@pennington.io>
2019-06-06 20:23:47 +0930

    fix(index): fixed issue where web app loads incorrectly

--- main.ts
+++ main.ts
@@ -1,76 +1,85 @@
-import { app, BrowserWindow, screen } from 'electron';
+import { app, BrowserWindow, screen, Screen, Size } from 'electron';
 import * as path from 'path';
 import * as url from 'url';
 
-let win, serve;
-const args = process.argv.slice(1);
-serve = args.some(val => val === '--serve');
+/**
+ * the browser window containing the electron app
+ */
+let win: BrowserWindow;
 
-function createWindow() {
+/**
+ * get the list of argumnts to determine if serving or built
+ */
+const args: Array<string> = process.argv.slice(1);
+const serve: boolean = args.some((arg: string) => arg === '--serve');
 
-  const electronScreen = screen;
-  const size = electronScreen.getPrimaryDisplay().workAreaSize;
+/**
+ * construct the electron window
+ */
+function createWindow(): void {
+    const electronScreen: Screen = screen;
+    const size: Size = electronScreen.getPrimaryDisplay().workAreaSize;
 
-  // Create the browser window.
-  win = new BrowserWindow({
-    x: 0,
-    y: 0,
-    width: size.width,
-    height: size.height,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-    frame: false
-  });
-
-  if (serve) {
-    require('electron-reload')(__dirname, {
-      electron: require(`${__dirname}/node_modules/electron`)
+    // Create the browser window.
+    win = new BrowserWindow({
+        x: 0,
+        y: 0,
+        width: size.width,
+        height: size.height,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        frame: false
     });
-    win.loadURL('http://localhost:4200');
-  } else {
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'dist/index.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
-  }
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store window
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
+    if (serve) {
+        // tslint:disable-next-line: no-require-imports
+        require('electron-reload')(__dirname, {
+            electron: require(`${__dirname}/node_modules/electron`)
+        });
+        win.loadURL('http://localhost:4200');
+    } else {
+        win.loadURL(
+            url.format({
+                pathname: path.join(__dirname, 'dist/index.html'),
+                protocol: 'file:',
+                slashes: true
+            })
+        );
+    }
 
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store window
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
 }
 
 try {
+    // This method will be called when Electron has finished
+    // initialization and is ready to create browser windows.
+    // Some APIs can only be used after this event occurs.
+    app.on('ready', createWindow);
 
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  app.on('ready', createWindow);
+    // Quit when all windows are closed.
+    app.on('window-all-closed', () => {
+        // On OS X it is common for applications and their menu bar
+        // to stay active until the user quits explicitly with Cmd + Q
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
+    });
 
-  // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
-  });
-
-  app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow();
-    }
-  });
-
+    app.on('activate', () => {
+        // On OS X it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (win === null) {
+            createWindow();
+        }
+    });
 } catch (e) {
-  // Catch Error
-  // throw e;
+    // Catch Error
+    // throw e;
 }
